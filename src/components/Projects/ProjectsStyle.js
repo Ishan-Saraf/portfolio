b22ba8e6b05b8b767a375ab3bba727e3dccf9ce1
Ishan-Saraf <ishaansaraf03@gmail.com>
2024-04-29 15:59:20 +0530

    Project Upload

---
+++ src/components/Projects/ProjectsStyle.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import _default from '../../themes/default';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding: 40px 0px 80px 0px;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+`;
+
+export const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 10px 0px 100px 0;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+export const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+export const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+export const Divider = styled.div`
+    width: 1.5px;
+    background: ${({ theme }) => theme.primary};
+`
+
+
+export const CardContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 28px;
+    flex-wrap: wrap;
+`;
---
+++ src/components/Projects/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+            Here are some of my projects.
+        </Desc>
+        <CardContainer>
+          {projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
